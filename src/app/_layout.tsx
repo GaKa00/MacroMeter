811fd1c21889a1cca8595c9f068e51ab0f5f46e5
Gabriel Kassarp <gkassarp@gmail.com>
2026-04-22 14:11:48 +0200

    added initial routing

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack  screenOptions={{headerTitle: 'MacroMeter', headerTitleAlign: 'center', headerShown: false}}/>;
+  return <Stack  screenOptions={{headerTitle: 'MacroMeter', headerTitleAlign: 'center', headerShown: false}}>;
+<Stack.Screen name="index" options={{headerTitle: 'Home', headerTitleAlign: 'center'}} />
+<Stack.Screen name="meals" options={{headerTitle: 'All Meals', headerTitleAlign: 'center'}} />
+  </Stack>;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,20 @@
 import { globalStyles } from "@/styles/global";
 import { Text, View, StyleSheet, Platform, ScrollView } from "react-native";
 import HomeHeader from "./components/HomeHeader";
+import { Link } from "expo-router";
 
 export default function HomeScreen() {
   return (
-    
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroMeter</Text>
-        <HomeHeader />
+      <HomeHeader />
+      <Link href='/meals' style={globalStyles.link}>
+        All Meals
+      </Link>
     </ScrollView>
   );
 }
+
+
 
 
